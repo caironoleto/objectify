db1a27cfb609263c305915b74a81ab6c9da771e2
Cairo Noleto <caironoleto@gmail.com>
2012-07-03 15:06:40 -0300

    Minor refactoring on Objectify::Rails::LogSubscriber

--- lib/objectify/rails/log_subscriber.rb
+++ lib/objectify/rails/log_subscriber.rb
@@ -4,47 +4,57 @@ module Objectify
   module Rails
     class LogSubscriber < ActiveSupport::LogSubscriber
       def start_processing(event)
-        logger.debug("    [Objectify] Started #{event.payload[:route]}")
+        debug("    [Objectify] Started #{event.payload[:route]}")
       end
 
       def inject(event)
-        if logger.debug?
-          object     = event.payload[:object]
-          method     = event.payload[:method]
-          parameters = event.payload[:parameters].map do |req, param|
-            param if req == :req
-          end.compact
-          arguments  = event.payload[:arguments].map(&:class).inspect[1..-2]
+        return unless debug?
 
-          message = "      [Injector] Invoking #{object}.#{method}(#{arguments}). "
-          message << duration(event)
+        object     = event.payload[:object]
+        method     = event.payload[:method]
+        parameters = event.payload[:parameters].map do |req, param|
+          param if req == :req
+        end.compact
+        arguments  = event.payload[:arguments].map(&:class).inspect[1..-2]
 
-          logger.debug(message)
-        end
+        message = "      [Injector] Invoking #{object}.#{method}(#{arguments}). "
+        message << duration(event)
+
+        debug(message)
       end
 
       def executor_start(event)
-        if logger.debug?
-          type = event.payload[:type].to_s.capitalize
-          logger.debug("    [#{type}] Executing #{event.payload[:name]} " + duration(event))
-        end
+        return unless debug?
+
+        type = event.payload[:type].to_s.capitalize
+        debug("    [#{type}] Executing #{event.payload[:name]} " + duration(event))
       end
 
       def executor(event)
-        if logger.debug?
-          type = event.payload[:type].to_s.capitalize
-          logger.debug("    [#{type}] Executed #{event.payload[:name]} " + duration(event))
-        end
+        return unless debug?
+
+        type = event.payload[:type].to_s.capitalize
+        debug("    [#{type}] Executed #{event.payload[:name]} " + duration(event))
       end
 
       def policy_chain_halted(event)
-        if logger.debug?
-          logger.debug("    [Policy] Chain halted at #{event.payload[:policy]}. Responding with #{event.payload[:responder]}." + duration(event))
-        end
+        return unless debug?
+
+        debug("    [Policy] Chain halted at #{event.payload[:policy]}. Responding with #{event.payload[:responder]}." + duration(event))
       end
 
-      def logger
-        ::Rails.logger
+      private
+
+      def logger(logger_object = ::Rails.logger)
+        logger_object
+      end
+
+      def debug(message)
+        logger.debug message
+      end
+
+      def debug?
+        logger.debug?
       end
 
       def duration(event)
